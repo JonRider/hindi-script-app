b0db58d1ae6be22673889112d0daf760a92b4601
JonRider <jon@rider.net>
2021-06-16 06:17:13 +0530

    Custom Admin Title

--- script/admin.py
+++ script/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Level, Word
 
+admin.site.site_header = 'Hindi Script App admin'
+admin.site.site_title = 'Hindi Script App admin'
+#admin.site.site_url = 'http://coffeehouse.com/'
+admin.site.index_title = 'Hindi Script App administration'
 
 # Register your models here.
 admin.site.register(Level)
